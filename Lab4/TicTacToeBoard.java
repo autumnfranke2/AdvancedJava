52b295192e72e97fdfa3d642650ae149d27030ed
Autumn <autumn2@sting.graceland.edu>
2018-03-07 22:04:18 -0600

    lots of work

--- Lab4/TicTacToeBoard.java
+++ Lab4/TicTacToeBoard.java
@@ -13,50 +13,43 @@ import java.awt.Font;
 
 public class TicTacToeBoard extends JPanel {
 	
-	public TicTacToeBoard() {
-	setSize(322,422);
-	setBackground(Color.white);
-	GameMouse gm = new GameMouse();
-	addMouseListener(gm);
-	
-
-}
-	//Game logic function
-	// who's turn
-	// state of a board
-	//redraw board based on state of change
-	//check for victory/loss or draw
-	//update instructions base on events
-	
+	private char snapGrid[][];
+	boolean isFull = true;
 	int titleFontSize = 36;
 	int InstructionsTitle = 24;
 	int Instructions = 12;
 	ArrayList<DrawX> xList = new ArrayList<DrawX>();
 	ArrayList<DrawO> oList = new ArrayList<DrawO>();
 	
+	public TicTacToeBoard() {
+	snapGrid = new char[3][3];
+	setSize(322,422);
+	setBackground(Color.white);
+	GameMouse gm = new GameMouse();
+	addMouseListener(gm);
+	}
+	
 
+	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		// font is (font name, font.style, size)
 		
 		//TODO: 
-		//update instructions
-		//x's and o's snap to grid
+		//alternate x's and o's
 		//draw a winning line
+		//Game logic function:
+			// who's turn
+			// state of a board
+			//redraw board based on state of change
+			//check for victory/loss or draw
+			//update instructions base on events
 		
 		
 		g.setFont(new Font("Arial", Font.PLAIN, titleFontSize)); //alignment on the x coordinate
 		g.drawString("Tic Tac Toe", 55 , ((50 - titleFontSize) / 2 + titleFontSize)); //coordinates represent bottom left of text
 		
-		
-		for (DrawX thisX : xList){
-			g.drawString("x", thisX.xCoord, thisX.yCoord);
-		}
-		
-		for (DrawO thisO : oList){
-			g.drawString("o", thisO.xCoord, thisO.yCoord);
-		}
 		Graphics2D g2 = (Graphics2D) g;
         Line2D Vline1 = new Line2D.Float(100, 75, 100, 325);
         g2.draw(Vline1);
@@ -77,26 +70,110 @@ public class TicTacToeBoard extends JPanel {
 		g.setFont(new Font("Arial", Font.PLAIN, Instructions)); //alignment on the x coordinate
 		g.drawString("Click where you want to place your character.", 25 , ((725 - Instructions) / 2 + Instructions));
 		g.drawString("First to get 3 in a row wins!", 75 , ((750 - Instructions) / 2 + Instructions));
-		
-		/*DrawX testX = new DrawX(200,200);
-		g.drawString("X", testX.xCoord, testX.yCoord);
-		
-		DrawO testO = new DrawO(200,200);
-		g.drawString("O", testO.xCoord, testO.yCoord);*/
-		
+	}
+	
+		//checks the board to see if its empty or not
+	public boolean FullBoard() {
+		for(int s = 0; s<3; s++) {
+			for(int q = 0; q<3; q++) {
+				if(snapGrid[s][q] == '0') {
+					isFull = false;
+				}
+			}		
 		}
+			return isFull;
+	}	
 	
 	
+	// checks for an overall win
+	private boolean Win() {
+		return(rowWin() || columnWin() || diagonalWin());
+	}
 	
+	
+	// if its 3 in a row
+	private boolean rowWin() {
+		for(int r = 0; r<3; r++) {
+			if(rowCol(snapGrid[r][1],snapGrid[r][2],snapGrid[r][3]) == true) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	// if its 3 in a column
+	private boolean columnWin() {
+		for(int r = 0; r<3; r++) {
+			if(rowCol(snapGrid[1][r],snapGrid[2][r],snapGrid[3][r]) == true) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	// if its 3 in a diagonal
+	private boolean diagonalWin() {
+		return(rowCol(snapGrid[1][1], snapGrid[2][2], snapGrid[3][3]) == true) & 
+				(rowCol(snapGrid[1][3], snapGrid[2][2], snapGrid[3][1]) == true);
+	}
+	
+	
+	private boolean rowCol(char rc1, char rc2, char rc3) {
+		return ((rc1 != '0') && (rc1 == rc2) && (rc2 == rc3));
+	}
 	
 	class GameMouse implements MouseListener{
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			System.out.println(e.getX() + "" + e.getY());
+			boolean XDraw = false;
+			
+			
+			if (e.getX()<=0 & e.getX()<=75) {
+				if(e.getY()<=150) {
+					
+				}
+				else if (e.getY()<=150 & e.getY()<=250){
+					
+				}
+				else{
+					
+				}
+			}
+			else if (e.getX()>=75 & e.getX()<=100) {
+				if(e.getY()<=150) {
+				}
+				else if (e.getY()<=150 & e.getY()<=250){
+					
+				}
+				else{
+					
+				}
+			}
+			else if (e.getX()>=100) {
+				if(e.getY()<=150) {
+				}
+				else if (e.getY()<=150 & e.getY()<=250){
+					
+				}
+				else{
+					
+				}
+			}
+			
+			if(XDraw = false) {
+				DrawX x = new DrawX(e.getX()-10, e.getY()+10);
+			}
+			else{
+				DrawO o = new DrawO(e.getX()-10, e.getY()+10);
+				XDraw = false;
+			}
+			/*System.out.println(e.getX() + "" + e.getY());
 			DrawX x = new DrawX(e.getX()-10, e.getY()+10);
 			xList.add(x);
-			repaint();
+			repaint();*/
 			
 		}
 
@@ -137,10 +214,16 @@ public class TicTacToeBoard extends JPanel {
 			this.yCoord = yCoord;
 		}
 	}
-	/*
-	 * class to alternate
-	 * while loop: while game isnt won alternate through x's and o's
-	 * if x starts alternate 9 times starting with x
-	 * if o starts alternate 9 times starting with o
-	 */
+	void DrawXs(Graphics g) {
+		for (DrawX thisX : xList){
+			g.drawString("x", thisX.xCoord, thisX.yCoord);
+		}
+	}
+	
+	
+	void DrawYs(Graphics g) {
+		for (DrawO thisO : oList){
+			g.drawString("o", thisO.xCoord, thisO.yCoord);
+		}
+	}
 }
